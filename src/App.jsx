748cd9cf3b88c9bab9a41b49b78ee372c74e5d69
Sergio Eder Cervantes Rincon <edercervantes66@gmail.com>
2025-07-17 18:35:59 -0600

    tailwind,  prettier and palette configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-5xl text-gray-400'>Hola mundo con Tailwind</h1>
+      <h1 className="text-5xl text-gray-400">Hola mundo con Tailwind</h1>
+
+      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Submit
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
